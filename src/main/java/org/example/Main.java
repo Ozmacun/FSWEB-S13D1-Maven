2f2320536d6d65390b7bfc35fcf4ac5f7de9931f
Ozmacun <149619575+Ozmacun@users.noreply.github.com>
2024-10-26 11:22:02 +0300

    Completed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,23 +23,23 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        if (clock < 0 || clock > 23 || !isBarking) {
+        if (clock < 0 || clock > 23) {
             return false;
         }
-        return (clock < 8 || clock > 20);
+        return isBarking && (clock < 8 || clock > 20);
     }
 
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-        return (firstAge > 13 && firstAge < 19) ||
-                (secondAge > 13 && secondAge < 19) ||
-                (thirdAge > 13 && thirdAge < 19);
+        return (firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19);
 
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
         int maxTemp = isSummer ? 45 : 35;
-        return temp < maxTemp && temp > 25;
+        return temp <= maxTemp && temp >= 25;
     }
 
     public static double area(double width, double height) {
